033fee28acc0685b0d4436dfa0b946aa0257a8a6
Terra-Git <544708735@qq.com>
2023-03-02 10:34:57 +0800

    mysqlclient

--- src/main/main.go
+++ src/main/main.go
@@ -4,8 +4,19 @@ import (
 	"mysql_client"
 )
 
+
 func main() {
-	client  := mysql_client.MysqlClient{}
-	client.Connet()
+	client := mysql_client.MysqlClient{}
+	mysql_user := mysql_client.MysqlUser{}
+	
+    mysql_user.Username_         = "terra"
+	mysql_user.Password_         = "kun123456"
+	mysql_user.Dp_ip_            = "127.0.0.1"
+	mysql_user.Dp_port_          = "3306"
+	mysql_user.Dbname_           = "test"
+	mysql_user.Max_connect_       = 10
+
+	client.Connect(mysql_user)
+
 	client.Test()
 }
--- src/mysql_client/mysql_client.go
+++ src/mysql_client/mysql_client.go
@@ -6,43 +6,35 @@ import(
 	_ "github.com/go-sql-driver/mysql"
 )
 
-
 type MysqlUser struct{
-	username_        string
-	password_        string
-	dp_ip_           string
-	dp_port_         string
-	dbname_          string
-	max_connet       int
-	max_free_connect int 
+	Username_          string
+	Password_          string
+	Dp_ip_             string
+	Dp_port_           string
+	Dbname_            string
+	Max_connect_       int 
 }
+
 // 实现增删改查
 type MysqlClient struct{
 	connect_  *sql.DB
 }
 
-func (client *MysqlClient) Connet(username string,password string,ip string,port string,dbName string,max_connet int,max_free_connect int){
-	client.connect_,err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", username, password, ip, port, dbName))
-	if err != nil {
-		return nil, err
-	}
+func (client *MysqlClient) Connet(username string,password string,ip string,port string,dbName string,max_connect int){
+	client.connect_,_ = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", username, password, ip, port, dbName))
+
 	// client.connect_,_ = sql.Open("mysql", "terra:kun123456@tcp(127.0.0.1:3306)/test")
-	//设置数据库最大连接数
-	client.connect_.SetConnMaxLifetime(max_connet)
+
 	//设置上数据库最大闲置连接数
-	client.connect_.SetMaxIdleConns(max_free_connect)
+	client.connect_.SetMaxIdleConns(max_connect)
 }
 
 func (client *MysqlClient) Connect( user MysqlUser){
-	client.connect_,err= sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",user.username_,user.password_,user.dp_ip_,user.dp_port_,user.dbname_))
-	if err != nil {
-		return nil, err
-	}
+	client.connect_,_= sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8",user.Username_,user.Password_,user.Dp_ip_,user.Dp_port_,user.Dbname_))
+
 	// client.connect_,_ = sql.Open("mysql", "terra:kun123456@tcp(127.0.0.1:3306)/test")
 	//设置数据库最大连接数
-	client.connect_.SetConnMaxLifetime(max_connet)
-	//设置上数据库最大闲置连接数
-	client.connect_.SetMaxIdleConns(max_free_connect)
+	client.connect_.SetMaxIdleConns(user.Max_connect_)
 }
 
 func (client *MysqlClient) query(){
